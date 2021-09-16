b7b1cbae902f2be381ca9294606195f2f32a361c
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-17 00:23:17 +0530

    Added Regex to match First name of the user

--- src/com/yml/javaregex/JavaRegexMain.java
+++ src/com/yml/javaregex/JavaRegexMain.java
@@ -1,7 +1,17 @@
 package com.yml.javaregex;
 
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
+
 public class JavaRegexMain {
     public static void main(String args[]) {
-        
+        PrintWriter out = new PrintWriter(System.out, true);
+
+        //Use Case 1 - First Name Matcher
+        String firstNamePattern = "^[A-Z]{1}[a-zA-Z]{2,}";
+        out.println(Pattern.matches(firstNamePattern, "Sujay")); // True Case
+        out.println(Pattern.matches(firstNamePattern, "1Abc"));  // False Case
+
+        out.close();
     }
 }
